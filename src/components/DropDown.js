d01d90282437e69d5d2a71382f90cd19f0583eb2
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2017-12-06 10:12:03 -0700

    added links to the dropdown quick links

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -12,14 +12,14 @@ class DropDown extends Component {
            <button className="dropbtn"><img className='studentPhoto'src="https://media-exp2.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAA3oAAAAJDAzNjUxYzliLTk5NGQtNGY3Yy05ODdkLTljMWIyZTVmNDQ3ZQ.jpg" alt=""/><i class="fa fa-sort-desc" aria-hidden="true"></i></button>
            <div className="dropdown-content">
             <a className="topLink" href="#">Edit Profile</a>
-            <a href="#">Cohort</a>
-            <a href="#">Members Portal</a>
-            <a href="#">Talent Portal</a>
-            <a href="#">Learn Curriculum</a>
-            <a href="#">LinkedIn</a>
-            <a href="#">Github</a>
-            <a href="#">Student Links</a>
-            <a className="bottomLink" href="#"><i class="fa fa-plus" aria-hidden="true"></i> Add Link</a>
+            <a href="https://www.linkedin.com/in/">Cohort</a>
+            <a href="https://www.linkedin.com/in/" target="_blank">Members Portal</a>
+            <a href="https://www.linkedin.com/in/" target="_blank">Talent Portal</a>
+            <a href="https://www.linkedin.com/in/" target="_blank">Learn Curriculum</a>
+            <a href="https://www.linkedin.com/in/" target="_blank">LinkedIn</a>
+            <a href="https://github.com/" target="_blank">Github</a>
+            <a href="https://www.linkedin.com/in/" target="_blank">Student Links</a>
+            <a className="bottomLink" href="https://www.linkedin.com/in/"><i class="fa fa-plus" aria-hidden="true"></i> Add Link</a>
            </div>
         </div>
       </div>
